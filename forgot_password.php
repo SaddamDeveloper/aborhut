18046703b77b0156be349d54a06c281246921f28
aash narayan mahato <ankm786@gmail.com>
2020-05-03 21:51:38 +0530

    forgot password page

---
+++ forgot_password.php
@@ -0,0 +1,76 @@
+<?php include('include/header.php'); ?>
+<!-- Sub banner start -->
+<div class="sub-banner overview-bgi">
+    <div class="overlay">
+        <div class="container">
+            <div class="breadcrumb-area">
+                <h1>Thank You</h1>
+                <ul class="breadcrumbs">
+                    <li><a href="./">Home</a></li>
+                    <li class="active">Thank You</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Sub Banner end -->
+
+<!-- Thank you start -->
+<!-- Content area start -->
+<div class="content-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <!-- Form content box start -->
+                <div class="form-content-box" style="margin: 30px auto;">
+                    <!-- details -->
+                    <div class="details">
+                        <!-- Main title -->
+                        <div class="main-title">
+                            <h1><span>Forgot</span> Password</h1>
+                        </div>
+                        <!-- Form start -->
+                        <form action="#" method="POST">
+                            <div class="form-group">
+                                <input type="text" name="email" class="input-text" placeholder="Email Address">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="button-md button-theme btn-block">Send Me Email</button>
+                            </div>
+                        </form>
+                        <!-- Form end -->
+                    </div>
+                    <!-- Footer -->
+                    <div class="footer">
+                        <span>
+                           I want to <a href="login.php">return to login</a>
+                        </span>
+                    </div>
+                </div>
+                <!-- Form content box end -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Content area end -->
+<!-- Thank you end -->
+
+<div class="clearfix" style="padding-top: 60px;"></div>
+<!-- Intro section -->
+<div class="intro-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-12">
+                <img src="img/logos/logo-2.png" alt="logo-2">
+            </div>
+            <div class="col-md-7 col-sm-6 col-xs-12">
+                <h3>Looking To Sell Or Rent Your Property?</h3>
+            </div>
+            <div class="col-md-2 col-sm-3 col-xs-12">
+                <a href="submit-property.html" class="btn button-md button-theme">Submit Now</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Intro end -->
+<?php include('include/footer.php'); ?>
